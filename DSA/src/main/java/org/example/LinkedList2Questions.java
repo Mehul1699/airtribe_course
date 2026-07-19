7308b6be51578353e783d7904a10685ce4fe2e74
Mehul1699 <mehul19356@gmail.com>
2026-07-19 22:05:34 +0530

    Linked Lists 2 - 12 July (Updated 19 July)

--- DSA/src/main/java/org/example/LinkedList2Questions.java
+++ DSA/src/main/java/org/example/LinkedList2Questions.java
@@ -438,6 +438,175 @@ public class LinkedList2Questions {
         }
     }
 
+    // ASSIGNMENT QUESTIONS
+    // Leetcode - Easy - 141. Linked List Cycle
+
+    /**
+     * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+     * <p>
+     * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+     * Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+     * <p>
+     * Return true if there is a cycle in the linked list. Otherwise, return false.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * Input: head = [3,2,0,-4], pos = 1
+     * Output: true
+     * Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+     * <p>
+     * Example 2:
+     * Input: head = [1,2], pos = 0
+     * Output: true
+     * Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+     * <p>
+     * Example 3:
+     * Input: head = [1], pos = -1
+     * Output: false
+     * Explanation: There is no cycle in the linked list.
+     */
+    public boolean hasCycle(Node head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;   // Slow and fast pointers meet if cycle exists
+            }
+        }
+        return false;
+    }
+
+    // Leetcode - Medium - 142. Linked List Cycle II
+
+    /**
+     * Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+     * <p>
+     * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+     * Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed).
+     * It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+     * <p>
+     * Do not modify the linked list.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * Input: head = [3,2,0,-4], pos = 1
+     * Output: tail connects to node index 1
+     * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+     * <p>
+     * Example 2:
+     * Input: head = [1,2], pos = 0
+     * Output: tail connects to node index 0
+     * Explanation: There is a cycle in the linked list, where tail connects to the first node.
+     * <p>
+     * Example 3:
+     * Input: head = [1], pos = -1
+     * Output: no cycle
+     * Explanation: There is no cycle in the linked list.
+     */
+    public Node detectCycle(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Node slow = head;
+        Node fast = head;
+        boolean isCycleExists = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                isCycleExists = true;
+                break;
+            }
+        }
+        if (!isCycleExists) {
+            return null;
+        }
+        // After meeting, take one pointer back to head and now both should move 1 at a time. They would eventually meet up
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    // Leetcode - Easy - 160. Intersection of Two Linked Lists
+
+    /**
+     * Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect.
+     * If the two linked lists have no intersection at all, return null.
+     * <p>
+     * For example, the following two linked lists begin to intersect at node c1:
+     * <p>
+     * <p>
+     * The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+     * <p>
+     * Note that the linked lists must retain their original structure after the function returns.
+     * <p>
+     * Custom Judge:
+     * <p>
+     * The inputs to the judge are given as follows (your program is not given these inputs):
+     * <p>
+     * intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+     * listA - The first linked list.
+     * listB - The second linked list.
+     * skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+     * skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+     * The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program.
+     * If you correctly return the intersected node, then your solution will be accepted.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+     * Output: Intersected at '8'
+     * Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+     * From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5].
+     * There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+     * - Note that the intersected node's value is not 1 because the nodes with value 1 in A and B (2nd node in A and 3rd node in B)
+     * are different node references. In other words, they point to two different locations in memory, while the nodes with value
+     * 8 in A and B (3rd node in A and 4th node in B) point to the same location in memory.
+     * <p>
+     * Example 2:
+     * Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+     * Output: Intersected at '2'
+     * Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+     * From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A;
+     * There are 1 node before the intersected node in B.
+     * <p>
+     * Example 3:
+     * Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+     * Output: No intersection
+     * Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect,
+     * intersectVal must be 0, while skipA and skipB can be arbitrary values.
+     * Explanation: The two lists do not intersect, so return null.
+     */
+    public Node getIntersectionNode(Node headA, Node headB) {
+        Node pointerA = headA;
+        Node pointerB = headB;
+        // When a pointer reaches the end, switch to other list. They will eventually meet. This will give us the answer.
+        while (pointerA != pointerB) {
+            if (pointerA == null) {
+                pointerA = headB;
+            } else {
+                pointerA = pointerA.next;
+            }
+            if (pointerB == null) {
+                pointerB = headA;
+            } else {
+                pointerB = pointerB.next;
+            }
+        }
+        return pointerA;
+    }
+
     static void main() {
         /*
         Node a = new Node(2);
